b9a9180726f61b3faa2da6a84c75d6c9dc906daa
lewie0chewie <36806467+lewie0chewie@users.noreply.github.com>
2018-02-24 22:46:08 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_VenturaLuis.py
@@ -0,0 +1,13 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Luis Ventura
+# 2/17/2018
+
+# Find the total sales
+totalSales = float(input("Enter the projected amount of total sales: "))
+
+# Calculate the total profit
+annualProfit = totalSales * .23
+
+# Display the profit
+print ("The profit will be: $", annualProfit)
